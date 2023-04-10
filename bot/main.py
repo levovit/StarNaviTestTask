e3ec7657a6cdacfb5ae9bfe7b12559ec437e6a63
levovit <playersoft1999@gmail.com>
2023-04-10 09:11:50 +0300

    case proceessed where you not entered bot token

--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,7 @@
-from handlers import bot
-
-
-if __name__ == '__main__':
+import telebot
+try:
+    from handlers import bot
     bot.infinity_polling()
+except telebot.apihelper.ApiTelegramException:
+    # if you don't provide token - service will just exit
+    pass
